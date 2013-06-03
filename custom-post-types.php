a50c1f0a64fcb6039fa74474246ee2a8d64cf048
stephen <stephen@welldonemarketing.com>
2013-06-03 14:21:07 -0400

    Added custom post type template

---
+++ custom-post-types.php
@@ -0,0 +1,49 @@
+<?php 
+
+//add_action( 'init', 'register_cpt_remc' );
+
+/* 
+
+function register_cpt_remc() {
+
+    $labels = array( 
+        'name' => _x( 'REMCs', 'remc' ),
+        'singular_name' => _x( 'REMC', 'remc' ),
+        'add_new' => _x( 'Add New', 'remc' ),
+        'add_new_item' => _x( 'Add New REMC', 'remc' ),
+        'edit_item' => _x( 'Edit REMC', 'remc' ),
+        'new_item' => _x( 'New REMC', 'remc' ),
+        'view_item' => _x( 'View REMC', 'remc' ),
+        'search_items' => _x( 'Search REMCs', 'remc' ),
+        'not_found' => _x( 'No REMCs found', 'remc' ),
+        'not_found_in_trash' => _x( 'No REMCs found in Trash', 'remc' ),
+        'parent_item_colon' => _x( 'Parent REMC:', 'remc' ),
+        'menu_name' => _x( 'REMCs', 'remc' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'hierarchical' => false,
+        
+        'supports' => array( 'title', 'custom-fields' ),
+        
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        
+        
+        'show_in_nav_menus' => false,
+        'publicly_queryable' => true,
+        'exclude_from_search' => true,
+        'has_archive' => false,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => false,
+        'capability_type' => 'post'
+    );
+
+    register_post_type( 'remc', $args );
+}
+*/
+
+?>
