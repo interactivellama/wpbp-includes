8b0473bf81c0e8b9bc57baead323b8a7f120adc1
stephen <stephen@welldonemarketing.com>
2013-07-22 14:35:02 -0400

    Removed remove_parent_class from all

--- _all.php
+++ _all.php
@@ -32,49 +32,6 @@ function llama_image ($image_id, $image_size = '') {
 	return $image_source;
 }
 
-// As of WP 3.1.1 addition of classes for css styling to parents of custom post types doesn't exist.
-// We want the correct classes added to the correct custom post type parent in the wp-nav-menu for css styling and highlighting, so we're modifying each individually...
-// The id of each link is required for each one you want to modify
-// Place this in your WordPress functions.php file
-
-function remove_parent_classes($class)
-{
-  // check for current page classes, return false if they exist.
-	return ($class == 'current_page_item' || $class == 'current_page_parent' || $class == 'active' || $class == 'current_page_ancestor'  || $class == 'current-menu-item') ? FALSE : TRUE;
-}
-
-function add_class_to_wp_nav_menu($classes)
-{
-     switch (get_post_type())
-     {
-     	case 'program':
-     		// we're viewing a custom post type, so remove the 'current_page_xxx and current-menu-item' from all menu items.
-     		// add the current page class to a specific menu item (replace ###).
-     		if (in_array('menu-item-83', $classes))
-     		{
-     		$classes = array_filter($classes, "remove_parent_classes");
-     		   //$classes[] = 'current_page_parent';
-         }
-     		break;
-
-     	case 'artist':
-     		// we're viewing a custom post type, so remove the 'current_page_xxx and current-menu-item' from all menu items.
-     		// add the current page class to a specific menu item (replace ###).
-     		if (in_array('menu-item-83', $classes))
-     		{
-     		$classes = array_filter($classes, "remove_parent_classes");
-     		   //$classes[] = 'current_page_parent';
-         }
-     		break;
-
-      // add more cases if necessary and/or a default
-     }
-	return $classes;
-}
-add_filter('nav_menu_css_class', 'add_class_to_wp_nav_menu');
-
-
-
 // no variable returns string, otherwise returns boolean
 
 function llama_page_template($file_name = '') {
