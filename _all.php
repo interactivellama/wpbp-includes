c7bba3f7ca15e3d5c5c63fa3bc9f49fb00b2f513
stephen <stephen@welldonemarketing.com>
2013-07-25 14:20:19 -0400

    Added comment

--- _all.php
+++ _all.php
@@ -26,6 +26,7 @@ include_once('htaccess.php');										// htaccess/url rewrite functions for ass
 Miscellaneous
 ======================= */
 
+// gets image source based on id and size
 function llama_image ($image_id, $image_size = '') {	
 	$image = wp_get_attachment_image_src($image_id, $image_size);
 	$image_source = $image[0];
