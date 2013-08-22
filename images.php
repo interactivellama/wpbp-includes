769341b5c484c30470e4c9f4c0c317406c86577c
Stephen James <llama@interactivellama.com>
2013-08-22 12:04:14 -0400

    Adding llama_get_image_by_id

--- images.php
+++ images.php
@@ -6,3 +6,9 @@ function llama_image ($image_id, $image_size = '') {
 	$image_source = $image[0];
 	return $image_source;
 }
+
+function llama_get_image_by_id ($image_id, $image_size = '') {	
+	$image = wp_get_attachment_image_src($image_id, $image_size);
+	$image_source = $image[0];
+	return $image_source;
+}
